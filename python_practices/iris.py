ee5627ebe18e226270ced6c71e2134947ad6acc1
André Canal <andredorigueto@gmail.com>
2025-08-30 10:55:29 -0300

    iris with logreg

---
+++ python_practices/iris.py
@@ -0,0 +1,32 @@
+
+#%% Load necessary libraries
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+#%% Load the Iris dataset from a CSV file
+# Load the Iris dataset from the UCI Machine Learning Repository
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+columns = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
+iris = pd.read_csv(url, header=None, names=columns)
+
+print(iris.head())
+
+#%% Split the dataset into features and target variable
+X = iris.drop("class", axis=1)
+y = iris["class"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+print("Train shape:", X_train.shape)
+print("Test shape:", X_test.shape)
+
+#%% Train Logistic Regression model
+
+clf = LogisticRegression()          # Instantiate model
+clf.fit(X_train, y_train)           # Fit the model to the dataset
+clf.predict(X_test)                 # Make predictions on the test set
+clf.predict_proba(X_test)           # See probabilities on the test set
+print(clf.score(X_test, y_test))    # Evaluate model using mean accuracy
+
+# %%
